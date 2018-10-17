f3adf9d4b48a6c14424646003833d70cc0479ad8
Håvard Aasmo <havi95@gmail.com>
2018-10-17 12:32:03 +0200

    Use FlatList in TaskList component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,17 +1,31 @@
 import React from "react";
-import { View } from 'react-native';
+import { View, FlatList, StyleSheet, Text } from 'react-native';
 import Task from "components/Task";
+import { dimensions, fonts, padding } from 'constants/Base';
 
 function TaskList({tasks}) {
   return (
-    <View>
-      {
-        tasks.map(
-          task => <Task key={task.id} title={task.title} description={task.description} status={task.status} />
-        )
-      }
-     </View>
+    <View style={styles.container}>
+        <FlatList
+          data={tasks}
+          renderItem={({item}) => <Text style={styles.item}>{item.title}</Text>}
+          keyExtractor={item => item.id.toString()}
+        />
+      </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: padding.sm,
+    fontSize: fonts.lg,
+    height: 50,
+    width: dimensions.fullWidth
+  },
+});
 
 export default TaskList;
